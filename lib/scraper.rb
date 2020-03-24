a553edd5babefb4545a4183e8130c5f985482c68
Tyler Croley <tjcroley@outlook.com>
2020-03-24 21:23:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
     page = Nokogiri::HTML(open(profile_url))
     scraped_student = {}
     
-    container = page.css(".social-icon-container")
+    container = page.css(".social-icon-container a")
 
     
   end
