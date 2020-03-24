a4b64741b80b61770c058c1c7b7c11c07d9f163a
Tyler Croley <tjcroley@outlook.com>
2020-03-24 21:12:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,17 +5,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-   scraped_students = []
-   html = Nokogiri::HTML(open(index_url))
-   html.css(".student-card").collect do |student|
-     hash = {
-       name: student.css("h4.student-name").text,
-       location: student.css("p.student-name").text,
-       profile_url: "http://students.learn.co/students/" + student.css("a").attribute("href")
-     }
-    scraped_students << hash
-  end
-  scraped_students
+    html = open('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+    scraped_students = []
+    
+    students = doc.css(".student-card")
+    students.each do |student|
+      
+      name: 
   end
   
 
