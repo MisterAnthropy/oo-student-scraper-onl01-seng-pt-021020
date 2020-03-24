458bcfcd0fd8b2a2be2b2608e384c7e188fc329c
Tyler Croley <tjcroley@outlook.com>
2020-03-24 21:28:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,8 +29,17 @@ class Scraper
     container.each do |link|
       if link.include?("twitter")
         student[:twitter] = link
-      elsif link.include?("")
-        student[:] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?(".com")
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = page.css(".profile-quote").text
+    student[:bio] = page.css(".")
+        
 
     
   end
