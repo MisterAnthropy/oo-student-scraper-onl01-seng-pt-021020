eb8ca1c9fc654d23c7afbdab1db6d9bba348de5c
Tyler Croley <tjcroley@outlook.com>
2020-03-24 20:33:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index_url = ""
+    index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
     
   end
 
